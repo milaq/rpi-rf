50af5c9c4743b74019a9cb5f851350939fa93799
milaq <micha.laqua@gmail.com>
2016-04-23 14:31:59 +0200

    add missing protocols
    also use cleaner protocol detection method.
    
    ported from https://github.com/sui77/rc-switch. all credit belongs there.

--- rpi_rf/rpi_rf.py
+++ rpi_rf/rpi_rf.py
@@ -4,12 +4,25 @@ Sending and receiving 433Mhz signals with cheap GPIO RF Modules on a Raspberry P
 
 import logging
 import time
+from collections import namedtuple
 
 from RPi import GPIO
 
 MAX_CHANGES = 67
 
 _LOGGER = logging.getLogger(__name__)
+
+Protocol = namedtuple('Protocol',
+                      ['pulselength',
+                       'sync_high', 'sync_low',
+                       'zero_high', 'zero_low',
+                       'one_high', 'one_low'])
+PROTOCOLS = (None,
+             Protocol(350, 1, 31, 1, 3, 3, 1),
+             Protocol(650, 1, 10, 1, 2, 2, 1),
+             Protocol(100, 30, 71, 4, 11, 9, 6),
+             Protocol(380, 1, 6, 1, 3, 3, 1),
+             Protocol(500, 6, 14, 1, 2, 2, 1))
 
 
 class RFDevice:
@@ -95,30 +108,27 @@ class RFDevice:
 
     def tx_l0(self):
         """Send a '0' bit."""
-        if self.tx_proto == 1:
-            return self.tx_waveform(1, 3)
-        elif self.tx_proto == 2:
-            return self.tx_waveform(1, 2)
-        else:
+        if not 0 < self.tx_proto < len(PROTOCOLS):
+            _LOGGER.error("Unknown TX protocol")
             return False
+        return self.tx_waveform(PROTOCOLS[self.tx_proto].zero_high,
+                                PROTOCOLS[self.tx_proto].zero_low)
 
     def tx_l1(self):
         """Send a '1' bit."""
-        if self.tx_proto == 1:
-            return self.tx_waveform(3, 1)
-        elif self.tx_proto == 2:
-            return self.tx_waveform(2, 1)
-        else:
+        if not 0 < self.tx_proto < len(PROTOCOLS):
+            _LOGGER.error("Unknown TX protocol")
             return False
+        return self.tx_waveform(PROTOCOLS[self.tx_proto].one_high,
+                                PROTOCOLS[self.tx_proto].one_low)
 
     def tx_sync(self):
         """Send a sync."""
-        if self.tx_proto == 1:
-            return self.tx_waveform(1, 31)
-        elif self.tx_proto == 2:
-            return self.tx_waveform(1, 10)
-        else:
+        if not 0 < self.tx_proto < len(PROTOCOLS):
+            _LOGGER.error("Unknown TX protocol")
             return False
+        return self.tx_waveform(PROTOCOLS[self.tx_proto].sync_high,
+                                PROTOCOLS[self.tx_proto].sync_low)
 
     def tx_waveform(self, highpulses, lowpulses):
         """Send basic waveform."""
@@ -159,14 +169,14 @@ class RFDevice:
         duration = timestamp - self._rx_last_timestamp
 
         if duration > 5000:
-            if self._rx_timings[0] + 200 > duration > self._rx_timings[0] - 200:
+            if duration - self._rx_timings[0] < 200:
                 self._rx_repeat_count += 1
                 self._rx_change_count -= 1
                 if self._rx_repeat_count == 2:
-                    if self._rx_waveform(1, self._rx_change_count, timestamp):
-                        _LOGGER.debug("RX code [protocol 1]: " + str(self.rx_code))
-                    elif self._rx_waveform(2, self._rx_change_count, timestamp):
-                        _LOGGER.debug("RX code [protocol 2]: " + str(self.rx_code))
+                    for pnum in range(1, len(PROTOCOLS)):
+                        if self._rx_waveform(pnum, self._rx_change_count, timestamp):
+                            _LOGGER.debug("RX code " + str(self.rx_code))
+                            break
                     self._rx_repeat_count = 0
             self._rx_change_count = 0
 
@@ -177,38 +187,29 @@ class RFDevice:
         self._rx_change_count += 1
         self._rx_last_timestamp = timestamp
 
-    def _rx_waveform(self, proto, change_count, timestamp):
+    def _rx_waveform(self, pnum, change_count, timestamp):
         """Detect waveform and format code."""
-        if proto == 1:
-            sync = 31
-            pulse = 3
-        elif proto == 2:
-            sync = 10
-            pulse = 2
-        else:
-            return False
         code = 0
-        delay = int(self._rx_timings[0] / sync)
-        delay_tolerance = delay * self.rx_tolerance * 0.01
+        delay = int(self._rx_timings[0] / PROTOCOLS[pnum].sync_low)
+        delay_tolerance = delay * self.rx_tolerance / 100
 
         for i in range(1, change_count, 2):
-            if (delay + delay_tolerance > self._rx_timings[i] > delay - delay_tolerance and
-                    delay * pulse + delay_tolerance > self._rx_timings[i+1] > delay * pulse - delay_tolerance):
+            if (self._rx_timings[i] - delay * PROTOCOLS[pnum].zero_high < delay_tolerance and
+                    self._rx_timings[i+1] - delay * PROTOCOLS[pnum].zero_low < delay_tolerance):
                 code <<= 1
-            elif (delay * pulse + delay_tolerance > self._rx_timings[i] > delay * pulse - delay_tolerance and
-                  delay + delay_tolerance > self._rx_timings[i+1] > delay - delay_tolerance):
-                code += 1
+            elif (self._rx_timings[i] - delay * PROTOCOLS[pnum].one_high < delay_tolerance and
+                  self._rx_timings[i+1] - delay * PROTOCOLS[pnum].one_low < delay_tolerance):
                 code <<= 1
+                code |= 1
             else:
-                code = 0
-        code >>= 1
+                return False
 
-        if change_count > 6 and code != 0:
+        if self._rx_change_count > 6 and code != 0:
             self.rx_code = code
             self.rx_code_timestamp = timestamp
             self.rx_bitlength = int(change_count / 2)
             self.rx_pulselength = delay
-            self.rx_proto = proto
+            self.rx_proto = pnum
             return True
 
         return False
